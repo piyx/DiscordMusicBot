045168ba6923e393a75370e9c49ab739130dcc3a
piyx <piyx.abhishek@gmail.com>
2020-09-24 16:46:45 +0530

    added secret file
    Former-commit-id: a5c2b9b586651e221988afc4e176f5619440594e

---
+++ utils/secret.py
@@ -0,0 +1,17 @@
+# Reddit secret
+
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+USER_AGENT = ""
+
+# Discord token
+TOKEN = ""
+
+# Weather api_key
+WEATHER_API = ""
+
+# Youtube api key
+YOUTUBE_API_KEY = ""
+
+# Nasa api
+NASA_API = ""
