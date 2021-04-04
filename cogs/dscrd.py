d73063c8a8d6140ee8c81d864c290a96e881b42f
piyx <piyx.abhishek@gmail.com>
2021-04-04 22:44:17 +0530

    update cogs
    Former-commit-id: 3ef2701fe0e80a11b85604103a994cf1b6517a71 [formerly a08732f0edb710a93ac57d98a54c24e1036d8768]
    Former-commit-id: ac1e1fe000bf585dd22b91fe7ef6a64b38a3471d

--- cogs/dscrd.py
+++ cogs/dscrd.py
@@ -2,13 +2,13 @@ import discord
 from discord.ext import tasks
 from discord.utils import get
 from discord.ext import commands
-from utils.embeds import wcm, bcm, error
+from utils.messages import message
 
 
 class Discord(commands.Cog):
     def __init__(self, client) -> None:
         self.client = client
-        self.status = "1101100"
+        self.status = "I'm a bot"
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -17,6 +17,7 @@ class Discord(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx):
+        '''Displays the ping'''
         embed = discord.Embed(
             title=f'Pong! {round(self.client.latency*1000)}ms', color=0x457832)
         await ctx.send(embed=embed)
@@ -29,39 +30,36 @@ class Discord(commands.Cog):
     async def on_member_remove(self, member):
         print(f"{member} has rage quit.")
 
-    async def change_status(self):
-        pass
-
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def delete(self, ctx, amount=5):
+        '''Deletes N number of messages'''
         await ctx.channel.purge(limit=amount)
 
     @commands.command()
     async def join(self, ctx):
+        '''Joins the channel of author'''
         if not ctx.author.voice:
-            return await ctx.send(f"{error} **`You need to be in a voice channel`**")
+            return await ctx.send(message("ERROR", "You need to be in a voice channel"))
 
         channel = ctx.author.voice.channel
         voice_client = ctx.voice_client
 
-        if voice_client and voice_client.is_playing():
-            return await ctx.send(f"{error} **`Can't use this command when playing music in a channel!`**")
-
         if voice_client:
-            await ctx.send(f"{wcm} **`Joined {channel}`**")
+            await ctx.send(message("CHECK", "Already in a voice channel"))
             return await ctx.voice_client.move_to(channel)
 
         await channel.connect()
-        await ctx.send(f"{wcm} **`Joined {channel}`**")
+        await ctx.send(message("SUCCESS", f"Joined {channel}"))
 
     @commands.command(aliases=['disconnect'])
     async def leave(self, ctx):
+        '''Leaves the current voice channel'''
         voice_client = ctx.voice_client
         if not voice_client:  # Check if bot instance is connected to voice channel
-            await ctx.send(f"{error} **`Not in any channel`**")
+            await ctx.send(message("CHECK", f"Not in any channel"))
         else:
-            await ctx.send(f"{wcm} **`Left {voice_client.channel}`**")
+            await ctx.send(message("CHECK", f"Left {voice_client.channel}"))
             await ctx.voice_client.disconnect()
 
 
--- cogs/questions.py
+++ cogs/questions.py
@@ -2,6 +2,12 @@ import discord
 import random
 from discord.ext import commands
 
+responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely",
+             "You may rely on it", "As I see it, yes", "Most Likely", "Outlook Good",
+             "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later",
+             "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+             "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very Doubtful"]
+
 
 class Question(commands.Cog):
     def __init__(self, client) -> None:
@@ -25,17 +31,12 @@ class Question(commands.Cog):
     async def boss(self, ctx):  # passing context
         '''Displays owner name'''
         embed = discord.Embed(
-            title=f'Nyx made me! He is my boss', color=0x457832)
+            title=f'v1rtu5l made me! He is my boss', color=0x457832)
         await ctx.send(embed=embed)
 
     @commands.command(aliases=['8ball', '8b', 'question'])
     async def ask(self, ctx, *, question):
         '''Replies to a question'''
-        responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely",
-                     "You may rely on it", "As I see it, yes", "Most Likely", "Outlook Good",
-                     "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later",
-                     "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
-                     "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very Doubtful"]
         embed = discord.Embed(
             title=f'Question: {question}', description=f"Answer: {random.choice(responses)}", color=0x36AFDF)
         await ctx.send(embed=embed)
