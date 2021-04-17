6cf9817647141497519fc842d899bc95eedffdc7
piyx <piyx.abhishek@gmail.com>
2021-04-17 16:13:10 +0530

    fix error msg
    Former-commit-id: 96e2c08079f5dd704f2d6dcefb738d2ae3d46e4b [formerly d7fb3df2a775bd3a0d273f5cc936eda9a63f8e5c]
    Former-commit-id: 97af68769a97816d8342404eff6aa89a7cec2790

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import os
-import discord
-from utils.secret import TOKEN
 from discord.utils import get
 from youtube_dl import YoutubeDL
 from discord.ext import commands
 from discord import FFmpegPCMAudio
 
+
 from utils.messages import message
+from utils.credentials import TOKEN
 
 # Discord token
 token = TOKEN
@@ -20,7 +20,7 @@ async def load(ctx, extension):
         bot.load_extension(f"cogs.{extension}")
         await ctx.send(message("SUCCESS", f"cogs.{extension} has been loaded"))
     except Exception as e:
-        await ctx.send(message("SUCCESS", e))
+        await ctx.send(message("ERROR", e))
 
 
 @bot.command()
@@ -30,7 +30,7 @@ async def unload(ctx, extension):
         bot.unload_extension(f"cogs.{extension}")
         await ctx.send(message("SUCCESS", f"cogs.{extension} has been unloaded"))
     except Exception as e:
-        await ctx.send(message("SUCCESS", e))
+        await ctx.send(message("ERROR", e))
 
 
 @bot.command()
@@ -39,9 +39,9 @@ async def reload(ctx, extension):
     try:
         bot.unload_extension(f"cogs.{extension}")
         bot.load_extension(f"cogs.{extension}")
-        await ctx.send(f":white_check_mark: **cogs.{extension} has been reloaded**")
+        await ctx.send(message("SUCCESS", f"cogs.{extension} has been reloaded"))
     except Exception as e:
-        await ctx.send(f":x: **{e}**")
+        await ctx.send(message("ERROR", e))
 
 
 # Load all cog files
@@ -55,7 +55,7 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send(message("ERROR", "Argument missing. Type .help <command> for usage details"))
 
-    elif isinstance(error, commands.CommandError):
-        await ctx.send(message("ERROR", f"Command does not exist. Type .help to list of commands"))
+    # elif isinstance(error, commands.CommandError):
+    #     await ctx.send(message("ERROR", f"Command does not exist. Type .help to list of commands"))
 
 bot.run(token)
